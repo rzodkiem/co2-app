b5fb964faff67a681f154e017fa3fdcbfa2a85ef
rzodkiem <rzodkiem@ee.pw.edu.pl>
2016-09-07 01:36:10 +0200

    Adding missing file

---
+++ src/main/groovy/com/rzodkiewicz/michal/enums/EmissionFilterType.groovy
@@ -0,0 +1,14 @@
+package com.rzodkiewicz.michal.enums
+
+
+enum EmissionFilterType {
+
+    COUNTRY,
+    SECTOR,
+    TIME,
+    COUNTRY_SECTOR,
+    COUNTRY_TIME,
+    SECTOR_TIME,
+    COUNTRY_SECTOR_TIME
+
+}
